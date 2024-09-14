bcfd010e10d8702d82d560681a6dcdd6c039e161
Rohit <rohitsaha128256@gmail.com>
2024-09-14 15:37:53 +0530

    added data transfromation pipleline

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from src.textSummarizer.logging import logging
 from src.textSummarizer.pipeline.s1_data_ingestion import DataIngestionPipeline
 from src.textSummarizer.pipeline.s2_data_validation import DataValidationPipeline
+from src.textSummarizer.pipeline.s3_data_transformation import DataTransformationPipeline
 from box.exceptions import BoxValueError
 
-STAGE_NAME = "Data Validation Stage"
+STAGE_NAME = "Data Transformation Stage"
 
 try:
     logging.info(f">>>>> stage{STAGE_NAME} started")
-    data_ingestion_obj = DataValidationPipeline()
-    data_ingestion_obj.main()
+    data_trans_obj = DataTransformationPipeline()
+    data_trans_obj.main()
     logging.info(f">>>>> stage{STAGE_NAME} completed")
 
 except BoxValueError as e:

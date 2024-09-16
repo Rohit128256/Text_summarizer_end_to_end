a5f4efea09a00230f9d99ec49ab4c0d9c206b666
Rohit <rohitsaha128256@gmail.com>
2024-09-16 18:41:37 +0530

    added model evaluation pipeline

--- main.py
+++ main.py
@@ -2,17 +2,75 @@ from src.textSummarizer.logging import logging
 from src.textSummarizer.pipeline.s1_data_ingestion import DataIngestionPipeline
 from src.textSummarizer.pipeline.s2_data_validation import DataValidationPipeline
 from src.textSummarizer.pipeline.s3_data_transformation import DataTransformationPipeline
+from src.textSummarizer.pipeline.s4_model_trainer import ModelTrainerPipeline
+from src.textSummarizer.pipeline.s5_model_evaluation import ModelEvaluationPipeline
 from box.exceptions import BoxValueError
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logging.info(f">>>>> stage - {STAGE_NAME} started")
+    data_ingestion_obj = DataIngestionPipeline()
+    data_ingestion_obj.main()
+    logging.info(f">>>>> stage - {STAGE_NAME} completed")
+
+except BoxValueError as e:
+    raise ValueError("problem happened in Data Ingestion")
+except Exception as e:
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logging.info(f">>>>> stage - {STAGE_NAME} started")
+    data_validation_obj = DataValidationPipeline()
+    data_validation_obj.main()
+    logging.info(f">>>>> stage - {STAGE_NAME} completed")
+
+except BoxValueError as e:
+    raise ValueError("problem happened in Data Validation")
+except Exception as e:
+    raise e
+
 
 STAGE_NAME = "Data Transformation Stage"
 
 try:
-    logging.info(f">>>>> stage{STAGE_NAME} started")
+    logging.info(f">>>>> stage - {STAGE_NAME} started")
     data_trans_obj = DataTransformationPipeline()
     data_trans_obj.main()
-    logging.info(f">>>>> stage{STAGE_NAME} completed")
+    logging.info(f">>>>> stage - {STAGE_NAME} completed")
 
 except BoxValueError as e:
-    raise ValueError("problem happened in data validation")
+    raise ValueError("problem happened in Data Transformation")
+except Exception as e:
+    raise e
+
+
+STAGE_NAME = "Model Training"
+
+try:
+    logging.info(f">>>>> stage - {STAGE_NAME} started")
+    model_tariner_obj = ModelTrainerPipeline()
+    model_tariner_obj.main()
+    logging.info(f">>>>> stage - {STAGE_NAME} completed")
+
+except BoxValueError as e:
+    raise ValueError("problem happened in Model Training")
+except Exception as e:
+    raise e
+
+
+STAGE_NAME = "Model Evaluation"
+
+try:
+    logging.info(f">>>>> stage - {STAGE_NAME} started")
+    model_evaluation_obj = ModelEvaluationPipeline()
+    model_evaluation_obj.main()
+    logging.info(f">>>>> stage - {STAGE_NAME} completed")
+
+except BoxValueError as e:
+    raise ValueError("problem happened in Model Evaluation")
 except Exception as e:
     raise e
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,6 @@
 from src.textSummarizer.constants import *
 from src.textSummarizer.utils.common import read_yaml, create_directories
-from src.textSummarizer.entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
+from src.textSummarizer.entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig, ModelEvaluationConfig
 from pathlib import Path
 
 class ConfigurationManager :
@@ -69,3 +69,18 @@ class ConfigurationManager :
             gradient_accumulation_steps = params.gradient_accumulation_steps
             )
         return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir = config.root_dir,
+            data_path = config.data_path,
+            model_path = config.model_path,
+            tokenizer_path = config.tokenizer_path,
+            metric_file_name = config.metric_file_name
+        )
+
+        return model_evaluation_config

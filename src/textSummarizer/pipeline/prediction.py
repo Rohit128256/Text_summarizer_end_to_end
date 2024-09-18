1df4b129510279f54372dacf26483f7162885bef
Rohit <rohitsaha128256@gmail.com>
2024-09-19 00:57:40 +0530

    added prediction pipeline and integrated with FastAPI

--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -8,8 +8,9 @@ class PredictionPipeline:
         self.config = ConfigurationManager().get_model_evaluation_config()
 
     def predict(self,text):
-        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+        tokenizer = AutoTokenizer.from_pretrained('google/pegasus-cnn_dailymail')
         gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
         pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
         output = pipe (text, **gen_kwargs)[0]["summary_text"]
         return output
+    
